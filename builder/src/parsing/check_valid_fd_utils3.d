c86894ca363c51f3f616b71c528d20c3d7401ec5
Samy BEN TAYEB <samybentayeb@outlook.com>
2025-01-07 09:55:25 +0100

    merge successfuly. need sorting

--- builder/src/parsing/check_valid_fd_utils3.d
+++
@@ -1,9 +0,0 @@
-builder/src/parsing/check_valid_fd_utils3.o: \
- src/parsing/check_valid_fd_utils3.c include/cub3d.h minilibx-linux/mlx.h \
- external/GNL/include/get_next_line.h external/DPRINTF/ft_printf.h \
- external/LIBFT/libft.h
-include/cub3d.h:
-minilibx-linux/mlx.h:
-external/GNL/include/get_next_line.h:
-external/DPRINTF/ft_printf.h:
-external/LIBFT/libft.h:
--- builder/src/parsing/get_value_fd.d
+++
@@ -1,9 +0,0 @@
-builder/src/parsing/get_value_fd.o: src/parsing/get_value_fd.c \
- include/cub3d.h minilibx-linux/mlx.h \
- external/GNL/include/get_next_line.h external/DPRINTF/ft_printf.h \
- external/LIBFT/libft.h
-include/cub3d.h:
-minilibx-linux/mlx.h:
-external/GNL/include/get_next_line.h:
-external/DPRINTF/ft_printf.h:
-external/LIBFT/libft.h:
